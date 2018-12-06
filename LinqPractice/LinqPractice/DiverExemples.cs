2faef52182de4e5300b362a815a8e5078dcdd312
Arslan Ahmad <ars.ahm6@gmail.com>
2018-12-06 12:11:51 +0500

    Scope limitation of range variables in fluent syntax and overcoming this limitation using the query syntax demostrated in QuerySyntaxVsFluent()

--- LinqPractice/LinqPractice/DiverExemples.cs
+++ LinqPractice/LinqPractice/DiverExemples.cs
@@ -14,7 +14,53 @@ namespace LinqPractice
             //GetDirectoryItemsQuery();
             //GetDirectoryItemsFluent();
             //EnrollmentCount();
-            SelectVsSelectMany();
+            //SelectVsSelectMany();
+            QuerySyntaxVsFluent();
+        }
+
+        static void QuerySyntaxVsFluent()
+        {
+            var source = new[] { "Harry James Potter", "Severus Snivilleus Snape", "Albus Wulfric Perceival Brian Dumbledore", "Dolores Janes Umbrdige", "Minevera McGonagall" };
+            // try printing the single names alongwith their full names sorted first according the fullname and then by name
+            // using query syntax is easier since many range variables can be in scope; this is not the case, however, with the fluent syntax.
+
+            var listUsingQuerySyntax = UsingQuerySyntax();
+            var listUsingFluentSyntax = UsingFluentSyntax();
+            Console.WriteLine("Printing list obtained using the query syntax: ");
+            PrintNames(listUsingQuerySyntax);
+            Console.WriteLine();
+            Console.WriteLine("Printing list obtained using the fluent syntax: ");
+            PrintNames(listUsingFluentSyntax);
+
+            List<(string name, string fullName)> UsingQuerySyntax()
+            {
+                return
+                    (from fullname in source
+                     from name in fullname.Split(' ')
+                     orderby fullname, name
+                     select (name, fullname)).ToList();
+                // That easy since both fullname and name are in scope
+            }
+
+            List<(string name, string fullName)> UsingFluentSyntax()
+            {
+                return source
+                        .SelectMany(fn => fn.Split(' ')
+                            .Select(n => new { Name = n, FullName = fn }))  // we had to this rola to overcome the scope limitation of fn
+                        .OrderBy(n => n.FullName)
+                        .ThenBy(n => n.Name)
+                        .Select(n => (n.Name, n.FullName))
+                        .ToList();
+                // See? So much rola to handle the scope limitation
+            }
+
+            void PrintNames(List<(string name, string fullname)> list)
+            {
+                foreach (var (name, fullname) in list)
+                {
+                    Console.WriteLine($"{name} came from {fullname}");
+                }
+            }
         }
 
         static void SelectVsSelectMany()
